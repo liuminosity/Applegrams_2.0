f301e1d7d3a418e02d12a04e79a43c5cd82941b8
MKS-Student <mks.student@hackreactor.com>
2015-08-27 13:21:23 -0700

    updated view to remove red and blue matrices

--- public/BoardView.js
+++ public/BoardView.js
@@ -1,15 +1,12 @@
 var BoardView = Backbone.View.extend({
 
-  initialize: function() {
+  initialize: function () {
     this.render();
 
     //this handles the clicking: first click on piece stores its data. The next click, if on another piece,
     //switches the two; if the second click is made on an empty spot, the original piece is simple moved there.
-    var X = Y = 0,
-      config = this.model,
-      that = this,
-      chop = false;
-    $('rect').on('click', function(event) {
+    var X = Y = 0, config = this.model, that = this, chop = false;
+    $('rect').on('click', function (event) {
       if (X === 0 && $(event.currentTarget).attr('fill') !== 'white') {
         X = Number($(event.currentTarget).attr('config_x'));
         Y = Number($(event.currentTarget).attr('config_y'));
@@ -41,13 +38,14 @@ var BoardView = Backbone.View.extend({
         }
       }
     });
-
+   
   },
 
-  render: function() {
+  render: function () {
     this.$el.html('');
 
-    this.spacing = 100;
+    this.spacing = 75;
+    this.matrix = this.model.matrix;
     this.width = this.model.width;
     this.height = this.model.height;
     d3.select('body').append('svg')
@@ -60,13 +58,12 @@ var BoardView = Backbone.View.extend({
   },
 
   //this function simply updates the View based off the state of the Model (the three matrices in BoardModel.js)
-  tileIt: function() {
+  tileIt: function () {
     d3.selectAll('rect').remove();
     d3.selectAll('text').remove();
     var redCount = blueCount = letterCount = doubleCheck = 0;
-    var letterMatrix = this.model.letterMatrix;
-    var blueLetters = this.model.blueLetterMatrix;
-    var redLetters = this.model.redLetterMatrix;
+    var matrix = this.model.matrix;
+
     for (var y = 0; y < this.height; y++) {
       for (var x = 0; x < this.width; x++) {
         d3.select('svg').append('rect').attr({
@@ -78,30 +75,30 @@ var BoardView = Backbone.View.extend({
           'config_y': y,
           'config_x': x
         });
-        if (letterMatrix[y][x] !== 0) {
+        if (matrix[y][x].letter !== 0) {
           letterCount++;
           //these two IF statements ensure lonely letters, in a column or row, will not be mistaken for invalid words
-          if (this.model.matrix(x, y - 1) === 0 && this.model.matrix(x, y + 1) === 0) {
-            redLetters[y][x] = 1;
+          if (this.model.letter(x, y-1) === 0 && this.model.letter(x, y+1) === 0) {
+            matrix[y][x].col = 1;
             doubleCheck++;
           }
-          if (this.model.matrix(x - 1, y) === 0 && this.model.matrix(x + 1, y) === 0) {
-            blueLetters[y][x] = 1;
+          if (this.model.letter(x-1, y) === 0 && this.model.letter(x+1, y) === 0) {
+            matrix[y][x].row= 1;
             doubleCheck++;
           }
           //this IF statement ensures that a letter completely on its own will stay yellow, and not be added to the total count of letters in valid places
           if (doubleCheck === 2) {
             redCount--;
             blueCount--;
-            redLetters[y][x] = 0;
-            blueLetters[y][x] = 0;
+            matrix[y][x].col = 0;
+            matrix[y][x].row= 0;
           }
           doubleCheck = 0;
           d3.select('svg').append('text').attr({
             'x': x * this.spacing + this.spacing * .15,
             'y': y * this.spacing + this.spacing * .80,
             'font-size': this.spacing
-          }).text(letterMatrix[y][x]);
+          }).text(matrix[y][x].letter);
           d3.select('svg').append('rect').attr({
             'x': x * this.spacing,
             'y': y * this.spacing,
@@ -114,7 +111,7 @@ var BoardView = Backbone.View.extend({
             'config_y': y,
             'config_x': x
           });
-          if (blueLetters[y][x] === 1) {
+          if (matrix[y][x].row=== 1) {
             blueCount++;
             d3.select('svg').append('rect').attr({
               'x': x * this.spacing,
@@ -129,7 +126,7 @@ var BoardView = Backbone.View.extend({
               'config_x': x
             });
           }
-          if (redLetters[y][x] === 1) {
+          if (matrix[y][x].col === 1) {
             redCount++;
             d3.select('svg').append('rect').attr({
               'x': x * this.spacing,
@@ -146,9 +143,7 @@ var BoardView = Backbone.View.extend({
             });
           }
           if (y === 0) {
-            JSON.stringify(this.model.redLetterMatrix);
             this.model.makeBigger('top');
-            JSON.stringify(this.model.redLetterMatrix);
             this.tileIt();
           }
           if (x === 0) {
@@ -167,41 +162,39 @@ var BoardView = Backbone.View.extend({
       }
     }
     //at the end of this long iteration, we check to see if the counts match,
-    //which would imply that all words are valid
+    //which would imply that all words are valid 
     if (redCount + blueCount === letterCount * 2) {
       this.checkIfConnected(letterCount);
     }
   },
 
   //after all the words have been validated, this step makes sure they are all a part of one big body (not separate)
-  checkIfConnected: function(letters) {
-    var letterMatrix = [];
-    for (var i = 0; i < this.model.letterMatrix.length; i++) {
-      letterMatrix.push(this.model.letterMatrix[i].slice());
+  checkIfConnected: function (letters) {
+    var matrix = [];
+    for (var i = 0; i < this.height; i++) {
+      matrix.push(this.model.matrix[i].slice());
     }
 
     for (i = 0; i < this.height; i++) {
       for (var j = 0; j < this.width; j++) {
-        if (letterMatrix[i][j] !== 0) {
+        if (matrix[i][j].letter !== 0) {
           var startPoint = [j, i];
           i += this.height;
           j += this.width;
         }
       }
     }
-    var count = 0;
+    var count = 0; 
     var model = this.model;
-
-    function crawler(point) {
-      var x = point[0],
-        y = point[1];
-      if (letterMatrix[y] && !!letterMatrix[y][x]) {
+    function crawler (point) { 
+      var x = point[0], y = point[1];
+      if (matrix[y][x] && matrix[y][x].letter !== 0) {
         count++;
-        letterMatrix[y][x] = 0;
-        crawler([x + 1, y]);
-        crawler([x - 1, y]);
-        crawler([x, y + 1]);
-        crawler([x, y - 1]);
+        matrix[y][x] = 0;
+        crawler([x+1, y]);
+        crawler([x-1, y]);
+        crawler([x, y+1]);
+        crawler([x, y-1]);
       }
     }
     crawler(startPoint);
@@ -210,25 +203,26 @@ var BoardView = Backbone.View.extend({
     }
   },
 
-  //temporary
-  completed: function() {
+  //temporary 
+  completed: function () {
     this.bite();
   },
 
   //places new piece below lowest, most to-the-right, current piece
-  bite: function() {
-    for (var i = this.height - 1; i >= 0; i--) {
-      for (var j = this.width - 1; j >= 0; j--) {
-        if (this.model.letterMatrix[i][j] !== 0) {
-          this.model.addPiece(2, i + 1, this.model.randomLetter());
+  bite: function () {
+    for (var i = this.height-1; i >= 0; i--) {
+      for (var j = this.width-1; j >= 0; j--) {
+        if (this.matrix[i][j].letter !== 0) {
+          this.matrix[i+1][2].letter = this.model.randomLetter();
+          this.model.colorWord(2, i+1);
           this.tileIt();
-          return i + 1;
+          return i+1; 
         }
       }
     }
   },
 
-  chop: function() {
+  chop: function () {
     var spot = this.bite();
     this.model.addPiece(3, spot, this.model.randomLetter());
     this.model.addPiece(4, spot, this.model.randomLetter());
